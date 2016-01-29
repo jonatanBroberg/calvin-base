f015ff3cf99db48217cc6ad90eadbb12fc17b7d5
pstahl <philip.stahl91@gmail.com>
2016-01-29 21:03:15 +0100

    refactor utils to requests

--- calvin/Tools/cscontrol.py
+++ calvin/Tools/cscontrol.py
@@ -17,24 +17,23 @@
 
 import argparse
 import json
-import calvin.utilities.utils as utils
-import os
 from calvin.utilities.calvinlogger import get_logger
-from calvin.utilities import calvinresponse
-import logging
+from calvin.requests.request_handler import RequestHandler
 
 _log = get_logger(__name__)
+_poster = RequestHandler()
 
 
 def control_id(args):
-    return utils.get_node_id(args.node)
+    return _poster.get_node_id(args.node)
 
 
 def get_node_info(control_uri, node_id):
     try:
-        return utils.get_node(control_uri, node_id)
+        return _poster.get_node(control_uri, node_id)
     except:
         raise Exception("No node with id {} found".format(node_id))
+
 
 def get_node_control_uri(control_uri, node_id):
     nodeinfo = get_node_info(control_uri, node_id)
@@ -44,7 +43,7 @@ def get_node_control_uri(control_uri, node_id):
 def requirements_file(path):
     """ Reads in a requirements file of JSON format with the structure:
         {<actor_name>: [(<req_op>, <req_args>), ...], ...}
-        
+
         Needs to be called after the initial deployment to get the actor_ids
     """
     reqs = None
@@ -57,51 +56,53 @@ def requirements_file(path):
         return {}
     return reqs
 
+
 def control_deploy(args):
     response = None
     print args
     reqs = requirements_file(args.reqs) if args.reqs else None
     try:
-        response = utils.deploy_application(args.node, args.script.name, args.script.read(), reqs, args.check)
+        response = RequestHandler().deploy_application(args.node, args.script.name, args.script.read(), reqs, args.check)
     except Exception as e:
         print e
     return response
 
+
 def control_actors(args):
     if args.cmd == 'list':
-        return utils.get_actors(args.node)
+        return _poster.get_actors(args.node)
     if args.cmd == 'info':
         if not args.id:
             raise Exception("No actor id given")
-        return utils.get_actor(args.node, args.id)
+        return _poster.get_actor(args.node, args.id)
     elif args.cmd == 'delete':
         if not args.id:
             raise Exception("No actor id given")
-        return utils.delete_actor(args.node, args.id)
+        return _poster.delete_actor(args.node, args.id)
     elif args.cmd == 'migrate':
         if not args.id or not args.peer_node:
             raise Exception("No actor or peer given")
-        return utils.migrate(args.node, args.id, args.peer_node)
+        return _poster.migrate(args.node, args.id, args.peer_node)
 
 
 def control_applications(args):
     if args.cmd == 'list':
-        return utils.get_applications(args.node)
+        return _poster.get_applications(args.node)
     elif args.cmd == 'delete':
         if not args.id:
             raise Exception("No application id given")
-        return utils.delete_application(args.node, args.id)
+        return _poster.delete_application(args.node, args.id)
 
 
 def control_nodes(args):
     from requests.exceptions import ConnectionError
     if args.cmd == 'list':
-        return utils.get_nodes(args.node)
+        return _poster.get_nodes(args.node)
     elif args.cmd == 'add':
-        return utils.peer_setup(args.node, *args.peerlist)
+        return _poster.peer_setup(args.node, *args.peerlist)
     elif args.cmd == 'stop':
         try:
-            return utils.quit(args.node)
+            return _poster.quit(args.node)
         except ConnectionError as e:
             # If the connection goes down before response that is OK
             return None
@@ -116,13 +117,13 @@ def control_storage(args):
         except:
             raise Exception("Malformed JSON index string:\n%s" % args.index)
         formated_index = format_index_string(index)
-        return utils.get_index(args.node, formated_index)
+        return _poster.get_index(args.node, formated_index)
     elif args.cmd == 'raw_get_index':
         try:
             index = json.loads(args.index)
         except:
             raise Exception("Malformed JSON index string:\n%s" % args.index)
-        return utils.get_index(args.node, index)
+        return _poster.get_index(args.node, index)
 
 
 def parse_args():
@@ -152,7 +153,7 @@ def parse_args():
     cmd_deploy.add_argument("script", metavar="<calvin script>", type=argparse.FileType('r'),
                             help="script to be deployed")
     cmd_deploy.add_argument('-c', '--no-check', dest='check', action='store_false', default=True,
-                           help='Don\'t verify if actors or components are correct, ' + 
+                           help='Don\'t verify if actors or components are correct, ' +
                                 'allows deployment of actors not known on the node')
 
     cmd_deploy.add_argument('--reqs', metavar='<reqs>', type=str,
