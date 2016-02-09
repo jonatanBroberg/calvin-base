1a2291a7bc6c1f25c6152d106ae32e69b52c25c5
pstahl <philip.stahl91@gmail.com>
2016-02-09 13:09:36 +0100

    possible to get least and most busy node

--- calvin/runtime/north/resource_manager.py
+++ calvin/runtime/north/resource_manager.py
@@ -1,3 +1,5 @@
+import sys
+
 from collections import defaultdict, deque
 
 from calvin.utilities.calvinlogger import get_logger
@@ -15,3 +17,25 @@ class ResourceManager(object):
     def register(self, node_id, usage):
         _log.debug("Registering resource usage for node {}: {}".format(node_id, usage))
         self.usages[node_id].append(usage)
+
+    def least_busy(self):
+        """Returns the id of the node with the lowest average CPU usage"""
+        min_usage, least_busy = sys.maxint, None
+        for node_id in self.usages.keys():
+            average = sum(self.usages[node_id]) / self.history_size
+            if average < min_usage:
+                min_usage = average
+                least_busy = node_id
+
+        return least_busy
+
+    def most_busy(self):
+        """Returns the id of the node with the highest average CPU usage"""
+        min_usage, most_busy = - sys.maxint, None
+        for node_id in self.usages.keys():
+            average = sum(self.usages[node_id]) / self.history_size
+            if average > min_usage:
+                min_usage = average
+                most_busy = node_id
+
+        return most_busy
