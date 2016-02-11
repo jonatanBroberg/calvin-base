87518481c0c78ddd7624d7d32e7821d8d6f47279
Jonatan Broberg <jonatan.broberg@gmail.com>
2016-02-11 15:51:55 +0100

    Fixes. Now it replicates a fix number of replicas for the lost actor

--- calvin/actor/connection.py
+++ calvin/actor/connection.py
@@ -6,3 +6,10 @@ class Connection(object):
         self.port_id = port_id
         self.peer_node_id = peer_node_id
         self.peer_port_id = peer_port_id
+
+    def __iter__(self):
+        yield("node_id", self.node_id)
+        yield("port_id", self.port_id)
+        yield("peer_node_id", self.peer_node_id)
+        yield("peer_port_id", self.peer_port_id)
+
