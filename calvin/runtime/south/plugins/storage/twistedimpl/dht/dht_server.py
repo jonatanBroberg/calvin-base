bb04edcb33c9727c1d576f102fc7b7ddfb3f3e96
pstahl <philip.stahl91@gmail.com>
2016-02-12 16:13:11 +0100

    remove ignore self flag

--- calvin/runtime/south/plugins/storage/twistedimpl/dht/dht_server.py
+++ calvin/runtime/south/plugins/storage/twistedimpl/dht/dht_server.py
@@ -147,7 +147,7 @@ class AutoDHTServer(StorageBase):
         dlist = []
         dlist.append(self.dht_server.bootstrap(bootstrap))
 
-        self._ssdps = SSDPServiceDiscovery(iface, ignore_self=False)
+        self._ssdps = SSDPServiceDiscovery(iface)
         dlist += self._ssdps.start()
 
         _log.debug("Register service %s %s:%s" % (network, ip, port))
