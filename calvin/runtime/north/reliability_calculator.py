f5e69ffed0241a1b6809a57cc728f0ea1f991186
Jonatan Broberg <jonatan.broberg@gmail.com>
2016-03-18 10:50:16 +0100

    Clerification in comments, reliability calculator

--- calvin/runtime/north/reliability_calculator.py
+++ calvin/runtime/north/reliability_calculator.py
@@ -8,20 +8,31 @@ class ReliabilityCalculator(object):
 
 	def calculate_reliability(self, failure_count, node_start_time, replication_time):
 		total_time = time.time() - node_start_time
-		MTBF = total_time/(failure_count + 1)
+		MTBF = 1000 * total_time/(failure_count + 1) #milliseconds
+
+		# For a Poisson process with a constant failure rate we get the probability of zero failures to occur in time t as:
+		p = math.exp(-float(replication_time)/MTBF)
+		return p
+
+		# Derivation to above formula
+		"""
+		Classic Poisson:
+		p = (math.exp(-_lambda) * (_lambda)^n) / (math.factorial(n))
+		p = probability that n failures occur when we have _lambda as the event rate, i.e. the average number of failures during time t
+
+		Zero failures gives:
+		p = math.exp(-_lambda)
+
+		Average number of failures during time t:
+		_lambda = (failure_count + 1)/(total_time) * 1000 * replication_time
+		_lambda = (1/MTBF) * replication_time
+
+		Conclusion:
+		p = math.exp(-replication_time/MTBF)
+		"""
 
 		# Weibull
 		"""
 		p = math.exp(-(time/delta)^beta)
 		Formula taken from "Efficient task replication and management for adaptive fault tolerance in Mobile Grid environments"
 		"""
-
-		#Poisson, failure distibution
-		"""
-		p = (math.exp(-fail_rate*time) * (fail_rate*time)^n) / (math.factorial(n))
-		If n=1 then probability that p is probability that one failure occur in time time  
-		"""
-
-		#Exponential
-		p = math.exp(-float(replication_time)/(1000*MTBF))
-		return p
