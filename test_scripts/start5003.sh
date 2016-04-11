72f3bfe5daf46f1d371b11788b8b0460181237b9
Jonatan Broberg <jonatan.broberg@gmail.com>
2016-04-11 11:20:08 +0200

    Remove start500x.sh files

--- test_scripts/start5003.sh
+++
@@ -1,2 +0,0 @@
-[ -f calvin.conf.bak ] && mv calvin.conf.bak calvin.conf
-csruntime --host 127.0.0.1 --port 5003 --controlport 5004 --keep-alive -f "localhost5003.log"
--- test_scripts/start5005.sh
+++
@@ -1,2 +0,0 @@
-[ -f calvin.conf.bak ] && mv calvin.conf.bak calvin.conf
-csruntime --host 127.0.0.1 --port 5005 --controlport 5006 --keep-alive -f "localhost5005.log"
