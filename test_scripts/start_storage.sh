0444b82a46c528a5414f02b8a2634664b88b38c6
Jonatan Broberg <jonatan.broberg@gmail.com>
2016-04-14 11:39:45 +0200

    Added port alternative to start_storage

--- test_scripts/start_storage.sh
+++ test_scripts/start_storage.sh
@@ -1,2 +1,10 @@
 [ -f calvin.conf ] && mv calvin.conf calvin.conf.bak
-csruntime --host `hostname -A | awk '{print $1}'` --port 4999 --controlport 5000 --keep-alive -s
+
+n=$1
+if [ -z "$n" ]; then
+    n=0
+fi
+port=$((4999+$n))
+controlport=$(($port+1))
+
+csruntime --host `hostname -A | awk '{print $1}'` --port $port --controlport $controlport --keep-alive -s
