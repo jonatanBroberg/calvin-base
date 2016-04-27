fe1175664db33ff62b2d6dff1784e5ff10ebd018
Jonatan Broberg <jonatan.broberg@gmail.com>
2016-04-27 19:50:24 +0200

    fix in print, only send times

--- calvin/runtime/north/calvin_node.py
+++ calvin/runtime/north/calvin_node.py
@@ -327,7 +327,7 @@ class Node(object):
             rel = rm.get_reliability(node_id, replication_times, failure_times)
             uri = rm.node_uris.get(node_id)
             if uri:
-                mtbf = rm.reliability_calculator.get_mtbf(failure_times)
+                mtbf = rm.reliability_calculator.get_mtbf(failure_times[uri])
                 rels.append((uri, rel, 1 - rel, mtbf))
         return rels
 
